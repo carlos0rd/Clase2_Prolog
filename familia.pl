234c43107517340709581d9e4eda3d7d1ca85dc5
José Alfredo Rodríguez <111230702+JoseRodriguezRodgriguez@users.noreply.github.com>
2025-09-22 15:57:57 -0600

    Create familia.pl

---
+++ familia.pl
@@ -0,0 +1,13 @@
+%1. vive_en(Persona, Lugar). (Carlos)
+
+%2. trabaja_en(Persona, Profesion). (Carlos)
+
+%3. padre(Padre, Hijo). / madre(Madre, Hijo). (Carlos)
+
+%4. casado_con(Persona1, Persona2). (Henry)
+
+%5. hijo(Hijo, Padre). (Henry)
+
+%6. amigo(Persona1, Persona2). (José)
+
+%7. primo(Persona1, Persona2). (José)
