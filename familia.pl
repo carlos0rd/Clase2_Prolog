27e85bb931301f11768b848015a0f657c615c427
José Alfredo Rodríguez <111230702+JoseRodriguezRodgriguez@users.noreply.github.com>
2025-09-22 16:07:45 -0600

    Parte de Henry y José

--- familia.pl
+++ familia.pl
@@ -5,9 +5,35 @@
 %3. padre(Padre, Hijo). / madre(Madre, Hijo). (Carlos)
 
 %4. casado_con(Persona1, Persona2). (Henry)
-
+casado_con(ernesto,teresa).
+casado_con(carlos,sofia).
+casado_con(ricardo,laura).
+casado_con(miguel,rosa).
 %5. hijo(Hijo, Padre). (Henry)
+%ernesto y teresa
+hijo(carlos,ernesto).
+hijo(ana,ernesto).
+hijo(lucia,ernesto).
+hijo(carlos,teresa).
+hijo(ana,teresa).
+hijo(lucia,teresa).
 
+%carlos y sofia
+hijo(mateo,carlos).
+hijo(valeria,carlos).
+hijo(mateo,sofia).
+hijo(valeria,sofia).
+
+%ricardo y laura
+hijo(daniel,ricardo).
+hijo(daniel,laura).
+
+%miguel y rosa
+hijo(fernanda,miguel).
+hijo(fernanda,rosa).
 %6. amigo(Persona1, Persona2). (José)
+amigo(carlos, ricardo).
+amigo(daniel, mateo).
 
 %7. primo(Persona1, Persona2). (José)
+primo(lucia, daniel).
